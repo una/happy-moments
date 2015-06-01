88ec13c54a692182c69fbc940c85f629330ae6c7
UNA A. KRAVETS <uakravet@us.ibm.com>
2015-06-01 12:32:02 -0400

    basic ractive setup

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+var ractive = new Ractive({
+  // The `el` option can be a node, an ID, or a CSS selector.
+  el: '#container',
+
+  // We could pass in a string, but for the sake of convenience
+  // we're passing the ID of the <script> tag above.
+  template: '#template',
+
+  // Here, we're passing in some initial data
+  data: { name: 'Una' }
+});
