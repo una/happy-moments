0a690a3b3a13d8a5e9646520c38eba8b474a2228
UNA A. KRAVETS <uakravet@us.ibm.com>
2015-06-02 21:19:32 -0500

    Pulling random date from API

--- app.js
+++ app.js
@@ -3,22 +3,24 @@
 var express = require('express');
 var app = express();
 var request = require("request");
-var json;
+var json, count, date, moment;
 
-// app.use('/', express.static(__dirname + '/', { extensions: ['html'] }));
-// app.use(express.static(__dirname + '/', { extensions: ['html'] }));
-// app.use(express.static(__dirname + 'public'));
-
-// get API stuff
+app.use(express.static(__dirname + '/', { extensions: ['html'] }));
+app.use(express.static(__dirname + 'public'));
 
 request.get("https://www.kimonolabs.com/api/2mf3bjrq?apikey=txL7B070QkLk5HPTGs915wNTSQP7jqi6",
 function(err, response, body) {
   json = JSON.parse(body);
+  count = json.count;
+  var n = Math.floor(Math.random()*count);
+  date = json.results.happyMoments[n].date.text;
+  moment = json.results.happyMoments[n].moment;
+  console.log(n, count, date, moment);
+
 });
 
 app.get('/', function(req, res){
-  res.send(json.results.happyMoments[0].date.text);
-  // res.send(json.results.happyMoments[0].moment);
+  res.send('./public/index.html');
 });
 
 var server = app.listen(5000, function () {
@@ -30,14 +32,14 @@ var server = app.listen(5000, function () {
 
 });
 
-
-// // Ractive stuff
+// Ractive stuff
 
 // var Ractive = require('ractive');
 
 // var ractive = new Ractive({
 //     template: 'hello from {{who}}'
 // });
+
 // ractive.set('who', 'node');
 // console.log(ractive.toHTML());
 
