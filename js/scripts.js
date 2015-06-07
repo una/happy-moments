3fa6cacd5020aec2e365c1f2123dd511a53c4be6
UNA A. KRAVETS <uakravet@us.ibm.com>
2015-06-07 12:49:34 -0500

    moved onload inside of request

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,21 @@
 var request = new XMLHttpRequest();
 request.open('GET', '/json-list', true);
 
-// if this is a link to a moment, load that first
-window.onload = function() {
-  console.log(window.location.href);
-
-  if (window.location.href.includes('#')) {
-    console.log(getHashNum(window.location.href));
-    updateMoments(getHashNum(window.location.href));
-    return;
-  }
-};
-
 request.onload = function() {
   if (request.status >= 200 && request.status < 400) {
     var data = JSON.parse(request.responseText);
     momentsFunction(data);
+
+    // if this is a link to a moment, load that first
+    window.onload = function() {
+      console.log(window.location.href);
+
+      if (window.location.href.includes('#')) {
+        console.log(getHashNum(window.location.href));
+        updateMoments(getHashNum(window.location.href));
+        return;
+      }
+    };
 
   } else {
     console.log('we can\'t find the happy moments :(');
