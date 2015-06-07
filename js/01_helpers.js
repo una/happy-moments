b5b713ab5f7d3e4ce503fd7dcc89a04bfab9e465
UNA A. KRAVETS <uakravet@us.ibm.com>
2015-06-07 12:57:45 -0500

    separated out scripts

---
+++ js/01_helpers.js
@@ -0,0 +1,24 @@
+// Date Formatting
+function formatDate(d) {
+  var months = {
+    '01' : 'Jan',
+    '02' : 'Feb',
+    '03' : 'Mar',
+    '04' : 'Apr',
+    '05' : 'May',
+    '06' : 'Jun',
+    '07' : 'Jul',
+    '08' : 'Aug',
+    '09' : 'Sept',
+    '10' : 'Oct',
+    '11' : 'Nov',
+    '12' : 'Dec'};
+  var monthNum = d.slice(0,2);
+  var dateNum = d.slice(3,5);
+
+  return (months[monthNum] + ' ' + dateNum);
+}
+
+function getHashNum(hash) {
+    return hash.split('#')[1];
+}
---
+++ js/04_moments.js
@@ -0,0 +1,47 @@
+// using list from API call
+function momentsFunction(data) {
+  var allMoments, count, date, moment, i;
+  allMoments = data;
+  count = allMoments.count;
+
+  if (window.location.href.includes('#')) {
+    console.log(getHashNum(window.location.href));
+    i = getHashNum(window.location.href);
+    updateMoments(i);
+  } else {
+    randomMoment();
+  }
+
+  function randomMoment() {
+    i = Math.floor(Math.random()*count);
+    updateMoments(i);
+  }
+
+  function nextMoment() {
+    i++;
+    updateMoments(i);
+  }
+
+  function prevMoment() {
+    i--;
+    updateMoments(i);
+  }
+
+  function updateMoments(i) {
+    date = allMoments.results.happyMoments[i].date.text;
+    moment = allMoments.results.happyMoments[i].moment;
+    document.querySelector('.moment--text').innerHTML = moment;
+    document.querySelector('.moment--date').innerHTML = formatDate(date);
+    history.pushState(null, null, '#' + i);
+  }
+
+  document.querySelector('.date--rand').addEventListener("click", function(e) {
+      randomMoment();
+    });
+  document.querySelector('.date--next').addEventListener("click", function(e) {
+      nextMoment();
+    });
+  document.querySelector('.date--prev').addEventListener("click", function(e) {
+      prevMoment();
+    });
+}
