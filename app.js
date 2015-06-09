4f55cd4934677f10a868de739f02ac1183536498
UNA A. KRAVETS <uakravet@us.ibm.com>
2015-06-09 10:35:55 -0500

    solved key. 404 broken

--- app.js
+++ app.js
@@ -4,25 +4,26 @@ var express = require('express');
 var app = express();
 var request = require('request');
 var fs = require('fs');
-var json, count, date, moment;
+var json, key, count, date, moment;
+
+//API Key
+fs.readFile('./key.txt', 'utf8', function (err,data) {
+  if (err) {
+    return console.log(err);
+  }
+  key = data;
+});
 
 app.use(express.static(__dirname + '/', { extensions: ['html'] }));
 app.use(express.static(__dirname + '/public'));
 
 // API Request
 app.get('/', function(req, res){
-  res.send('./public/index.html');
+  res.send('index.html');
 });
 
+// creating json list asset
 app.get('/json-list', function(req, res){
-  fs.readFile('./key.txt', 'utf8', function (err,data) {
-    if (err) {
-      return console.log(err);
-    }
-    var key = data;
-    console.log(key);
-  });
-
   request.get('https://www.kimonolabs.com/api/2mf3bjrq?apikey='+ key,
   function(err, response, body) {
     json = JSON.parse(body);
@@ -32,7 +33,7 @@ app.get('/json-list', function(req, res){
 
 // sending every url that doesn't exist
 app.get('*', function(req, res){
-  res.send('Wah 404 :(')
+  res.send('404.html');
 });
 
 app.listen(process.env.PORT || 3000, function(){
