07621c8af9f6cf9acd793d174170cfd359bec5d1
UNA A. KRAVETS <uakravet@us.ibm.com>
2015-08-06 20:56:08 -0500

    fixed a bug where the hash wasnt being read

--- js/keybindings.js
+++ js/keybindings.js
@@ -2,25 +2,21 @@
 module.exports = function(momentChanger) {
   'use strict';
   var unicode;
-  console.log(momentChanger.randomMoment());
 
   document.onkeydown = function(e) {
     e.preventDefault();
-    unicode = e.keyCode ? e.keyCode : e.charCode;
-    console.log(unicode);
+    // same as : unicode = e.keyCode ? e.keyCode : e.charCode;
+    unicode = e.keyCode || e.charCode;
 
     if(unicode === 32) {
-      console.log('space');
       momentChanger.randomMoment();
     }
 
     if(unicode === 39) {
-      console.log('rarr');
       momentChanger.nextMoment();
     }
 
     if(unicode === 37) {
-      console.log('larr');
       momentChanger.prevMoment();
     }
   };
